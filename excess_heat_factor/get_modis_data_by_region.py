f81be2f99670be77b75af71e5f5189487716e730
Hao Chen <chen.h@unimelb.edu.au>
2023-06-15 18:06:48 +1000

    updated modis script

--- excess_heat_factor/get_modis_data_by_region.py
+++ excess_heat_factor/get_modis_data_by_region.py
@@ -1,102 +1,98 @@
 # -*- coding: utf-8 -*-
 
-from datetime import datetime, timedelta
-
 import ee
 import geopandas as gpd
 import pandas as pd
 import xarray as xr
+from shapely.geometry import Polygon, MultiPolygon
 from tqdm import tqdm
 
 
 class ModisDataProcessor:
-    def __init__(self, region_collection_shp, geo_level_column, start_date, end_date, scale):
+    def __init__(self, product, region_collection_shp, geo_level_column, start_date, end_date, scale):
+        # ee.Authenticate()
         ee.Initialize()
 
-        self.region_collection_gdf = gpd.read_file(region_collection_shp)
+        self.product = product
+        self.region_collection_gdf = gpd.read_file(region_collection_shp)[:1]
         self.study_area = self.compute_study_area()
         self.geo_level_column = geo_level_column
-        self.region_collection_ee_feature = self.convert_ee_feature()
         self.start_date = start_date
         self.end_date = end_date
         self.scale = scale
+        self.data = []
 
     def compute_study_area(self):
-        """Compute the minimum bounding box of the region collection as an ee Polygon."""
         minx, miny, maxx, maxy = self.region_collection_gdf.total_bounds
         coords = [[minx, miny], [minx, maxy], [maxx, maxy], [maxx, miny], [minx, miny]]
         bounding_box = ee.Geometry.Polygon(coords)
         return bounding_box
 
-    def create_feature(self, polygon, row):
-        polygon = ee.Geometry.Polygon(list(polygon.exterior.coords))
-        feature = ee.Feature(polygon, {self.geo_level_column: row[self.geo_level_column]})
-        return feature
-
-    def convert_ee_feature(self):
-        """Convert the regions in region_collection_gdf to a list of ee features."""
-        feature_list = []
-        for _, row in self.region_collection_gdf.iterrows():
-            geom = row['geometry']
-
-            # Check if the geometry is a MultiPolygon
-            if geom.geom_type == 'MultiPolygon':
-                for polygon in geom.geoms:
-                    feature = self.create_feature(polygon, row)
-                    feature_list.append(feature)
-            else:  # It's a normal Polygon
-                feature = self.create_feature(geom, row)
-                feature_list.append(feature)
-
-        return ee.FeatureCollection(feature_list)
+    @staticmethod
+    def shapely_to_ee(geom):
+        geojson = geom.__geo_interface__
+        if isinstance(geom, Polygon):
+            return ee.Geometry.Polygon(geojson['coordinates'])
+        elif isinstance(geom, MultiPolygon):
+            return ee.Geometry.MultiPolygon(geojson['coordinates'])
 
     @staticmethod
     def convert_to_celsius(image):
         return image.select(['LST_Day_1km', 'LST_Night_1km']).multiply(0.02).subtract(273.15)
 
-    def reduce_region(self, image):
-        return image.reduceRegion(self.region_collection_ee_feature, ee.Reducer.minMax(), self.scale)
-
     def fetch_data(self):
-        image_collection = ee \
-            .ImageCollection("MODIS/006/MOD11A2") \
-            .filterDate(self.start_date, self.end_date) \
+        image_collection = ee\
+            .ImageCollection(self.product)\
+            .filterDate(self.start_date, self.end_date)\
             .filterBounds(self.study_area) \
-            .map(self.convert_to_celsius) \
-            .select(['LST_Day_1km', 'LST_Night_1km'])
+            .map(self.convert_to_celsius)
 
-        self.start_date = datetime.strptime(self.start_date, '%Y-%m-%d')
-        self.end_date = datetime.strptime(self.end_date, '%Y-%m-%d')
-        delta = self.end_date - self.start_date
+        for _, row in tqdm(self.region_collection_gdf.iterrows(),
+                           total=self.region_collection_gdf.shape[0],
+                           desc=f"Iterating over regions from {self.geo_level_column}"):
+            region_id = row[self.geo_level_column]
+            geom = self.shapely_to_ee(row['geometry'])
+            data = image_collection.getRegion(geom, self.scale).getInfo()
+            df = pd.DataFrame(data)
+            print(df)
 
+    def save_shapefile(self, file_name):
+        flat_data = [item for sublist in self.data for item in sublist]
+        df = pd.DataFrame(flat_data)
+        df.set_index('region_id', inplace=True)
 
-        # Process data day by day to avoid large payload size
-        for i in tqdm(range(delta.days + 1):
-            # Calculate the date for the current day
-            day = self.start_date + timedelta(days=i)
+        reshaped_df = pd.DataFrame()
+        for date_col in df.columns:
+            date = pd.to_datetime(date_col)
+            day_max_col = f"{date_col}_DayMax"
+            night_min_col = f"{date_col}_NightMin"
+            reshaped_df[day_max_col] = df[date_col].apply(lambda x: x.get('LST_Day_1km'))
+            reshaped_df[night_min_col] = df[date_col].apply(lambda x: x.get('LST_Night_1km'))
 
-            # Filter the data for the current day
-            day_collection = image_collection.filter(ee.Filter.calendarRange(day.day, day.day, 'day_of_year'))
+        gdf = self.region_collection_gdf[[self.geo_level_column, 'geometry']].\
+            merge(reshaped_df, left_on=self.geo_level_column, right_index=True)
+        gdf.to_file(file_name, driver='ESRI Shapefile')
 
-            # Reduce the data to the regions
-            daily_data = day_collection.map(self.reduce_region)
-
-            # Fetch data for each day individually
-            daily_data_info = daily_data.getInfo()
-            result.append(daily_data_info)
+    def save_netcdf(self, file_name):
+        flat_data = [item for sublist in self.data for item in sublist]
+        df = pd.DataFrame(flat_data)
+        df.set_index('region_id', inplace=True)
+        xds = xr.Dataset.from_dataframe(df)
+        xds.to_netcdf(file_name)
 
 
 if __name__ == '__main__':
-    def main():
-        region_collection_shp = "../_data/boundary_data/sa4_nsw.shp"
-        geo_level_column = "SA4_CODE16"
-        start_date = "2019-07-01"
-        end_date = "2019-07-05"
-        scale = 100
-        save_file_name = "modis_data.nc"
+    product = "MODIS/006/MOD11A1"
+    region_collection_shp = "../_data/boundary_data/sa4_nsw.shp"
+    geo_level_column = "SA4_CODE16"
+    start_date = "2020-01-02"
+    end_date = "2020-01-06"
+    scale = 500
+    shapefile_name = f"{geo_level_column}_{start_date}_{end_date}.shapefile"
+    netcdf_name = f"{geo_level_column}_{start_date}_{end_date}.nc"
 
-        processor = ModisDataProcessor(region_collection_shp, geo_level_column, start_date, end_date, scale)
-        processor.fetch_data()
-        processor.save_as_netcdf(save_file_name)
-
-    main()
+    processor = ModisDataProcessor(product, region_collection_shp, geo_level_column,
+                                   start_date, end_date, scale)
+    processor.fetch_data()
+    # processor.save_shapefile(shapefile_name)
+    # processor.save_netcdf(netcdf_name)
