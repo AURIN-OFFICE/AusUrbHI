1689ebd7fa83ef439b492381be5012cc5036107f
Hao Chen <chen.h@unimelb.edu.au>
2023-06-27 17:15:54 +1000

    updated study area refinement

--- aurin_datasets/HVI/refine.py
+++ aurin_datasets/HVI/refine.py
@@ -1,6 +1,7 @@
 import os
 import geopandas as gpd
 from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
 
 
 def refine_abs_2016():
@@ -8,12 +9,18 @@ def refine_abs_2016():
     boundary_data = gpd.read_file(boundary_file)
     boundary_matching_column = "SA1_MAIN16"
     assert boundary_matching_column in boundary_data.columns
+
+    data_folder = "../../_data/AusUrbHI HVI data raw/ABS 2016/"
+    data_files = [f for f in os.listdir(data_folder) if f.endswith('.json')]
+    assert len(data_files) == 18
     data_matching_column = "sa1_main16"
 
-    data_files = os.listdir("../../_data/AusUrbHI HVI data raw/ABS 2016")
-    for data_file in tqdm(data_files, desc="Refining data"):
-        data = gpd.read_file("../../_data/AusUrbHI HVI data raw/ABS 2016/" + data_file)
+    for data_file in tqdm(data_files, desc="refine_abs_2016"):
+        data = gpd.read_file(data_folder + data_file)
         assert data_matching_column in data.columns
+        assert data['data_matching_column'].isin(boundary_data['boundary_matching_column']).all(), \
+            f"Not all values from {data_matching_column} are present."
+
         output_file = "../../_data/AusUrbHI HVI data study area refined/ABS 2016" \
                       + data_file[45:].split('.')[0] + ".shp"
         refined_data = data[data[data_matching_column].isin(boundary_data[boundary_matching_column])]
@@ -25,20 +32,22 @@ def refine_abs_2021():
     boundary_data = gpd.read_file(boundary_file)
     boundary_matching_column = "SA1_MAIN16"
     assert boundary_matching_column in boundary_data.columns
-    data_matching_column = "sa1_main16"
 
-    data_files = os.listdir("../../_data/AusUrbHI HVI data raw/ABS 2016")
-    for data_file in tqdm(data_files, desc="Refining data"):
-        data = gpd.read_file("../../_data/AusUrbHI HVI data raw/ABS 2016/" + data_file)
+    data_folder = "../../_data/AusUrbHI HVI data raw/ABS 2021/"
+    data_files = [f for f in os.listdir(data_folder) if f.endswith('.shp')]
+    assert len(data_files) == 18
+    data_matching_column = "SA1_CODE_2"
+
+    for data_file in tqdm(data_files, desc="refine_abs_2021"):
+        data = gpd.read_file(data_folder + data_file)
         assert data_matching_column in data.columns
-        output_file = "../../_data/AusUrbHI HVI data study area refined/ABS 2016" \
-                      + data_file[45:].split('.')[0] + ".shp"
+        assert data['data_matching_column'].isin(boundary_data['boundary_matching_column']).all(), \
+            f"Not all values from {data_matching_column} are present."
+
+        output_file = "../../_data/AusUrbHI HVI data study area refined/ABS 2021" \
+                      + data_file[5:].split('.')[0] + ".shp"
         refined_data = data[data[data_matching_column].isin(boundary_data[boundary_matching_column])]
         refined_data.to_file(output_file)
-
-
-def refine_osm():
-    pass
 
 
 def refine_other_abs():
@@ -51,4 +60,7 @@ def phidu_natsem():
 
 if __name__ == "__main__":
     # refine_abs_2016()
-    refine_abs_2021()
+    # refine_abs_2021()
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        executor.submit(refine_abs_2016)
+        executor.submit(refine_abs_2021)
