b983cb86110d23332db5837327cbdbe3455a5787
Hao Chen <chen.h@unimelb.edu.au>
2023-08-08 01:01:00 +1000

    add EHF function

--- excess_heat_factor/masking_ehf.py
+++ excess_heat_factor/masking_ehf.py
@@ -1,59 +1,48 @@
-import netCDF4 as nc
+import netCDF4
+import geopandas as gpd
 import numpy as np
-import xarray as xr
 import rasterio
 from rasterio.features import geometry_mask
-from shapely.geometry import shape
-import geopandas as gpd
 from tqdm import tqdm
-from osgeo import gdal, ogr, osr
-
-gdf = gpd.read_file('../_data/study area/ausurbhi_study_area_2021.shp')
-dataset = nc.Dataset('../_data/longpaddock_silo_lst/EHF_heatwaves____daily.nc')
-# 1459; 7844
-
-# Assume that the data variable is named 'precipitation'
-data = dataset.variables['EHF'][1459]
-
-# Create a raster dataset in memory
-mem_drv = gdal.GetDriverByName('MEM')
-out_ds = mem_drv.Create('', data.shape[1], data.shape[0], 1, gdal.GDT_Float32)
-
-# Write data to the raster dataset
-out_band = out_ds.GetRasterBand(1)
-out_band.WriteArray(data)
-
-# Set up spatial reference system (SRS)
-srs = osr.SpatialReference()
-# The projection could be different depending on your data
-srs.ImportFromEPSG(7844)
-out_ds.SetProjection(srs.ExportToWkt())
-
-# Convert raster to vector
-mem_drv = ogr.GetDriverByName('Memory')
-out_ds2 = mem_drv.CreateDataSource('out')
-out_layer = out_ds2.CreateLayer('polygonized', srs=srs)
-gdal.Polygonize(out_band, None, out_layer, -1, [], callback=None)
-
-# Convert to a geopandas dataframe
-geoms = []
-for feature in out_layer:
-    geom = feature.GetGeometryRef()
-    geoms.append(geom.ExportToWkt())
-
-gdf = gpd.GeoDataFrame(geoms, columns=['geometry'])
 
 
-# Add function to check if geometry has Z coordinate
-def check_3d(geom):
-    # Checking if 'Z' is in the WKT string representation of the geometry
-    return ' Z ' in geom
+def netcdf_to_shp(nc_file, variable, output_path):
+    # Load the NetCDF file
+    nc_data = netCDF4.Dataset(nc_file)
+    ehf_data = nc_data.variables[variable][:]
+
+    # Load the shapefile
+    shapefile_path = '..\\_data\\study area\\ausurbhi_study_area_2021.shp'
+    shapes = gpd.read_file(shapefile_path)[:10]
+
+    # Create a list to store the maximum values
+    max_values = []
+
+    # Loop through the geometries in the shapefile
+    for geometry in tqdm(shapes['geometry'], desc='Masking data', total=len(shapes)):
+        # Create a mask from the geometry
+        mask = geometry_mask([geometry], out_shape=ehf_data.shape[1:], transform=rasterio.Affine(1, 0, 0, 0, -1, 0),
+                             invert=True)
+
+        # Apply the mask to the EHF data
+        masked_data = np.ma.array(ehf_data, mask=np.broadcast_to(mask, ehf_data.shape))
+
+        # Find the maximum value within the geometry
+        max_value = masked_data.max(axis=(1, 2)).tolist()
+        max_values.append(max_value)
+
+    # Add the maximum values to the shapefile
+    shapes['max_values'] = max_values
+
+    # Convert the maximum values to strings
+    max_values_str = [','.join(map(str, values)) for values in max_values]
+
+    # Add the maximum values to the shapefile
+    shapes['max_values'] = max_values_str
+
+    # Save the new shapefile
+    shapes.to_file(output_path)
 
 
-# Add a new column 'has_z' to the DataFrame
-gdf['has_z'] = gdf['geometry'].apply(check_3d)
-
-
-# Convert to a shapefile
-gdf.to_file('../_data/longpaddock_silo_lst/new_shapefile.shp')
-
+netcdf_to_shp("..\\_data\\AusUrbHI HVI data unprocessed\\Longpaddock SILO LST\\EHF_heatwaves____daily.nc",
+              "EHF", "..\\_data\\AusUrbHI HVI data processed\\Longpaddock SILO LST\\ehf.shp")
--- excess_heat_factor/refine_and_buff_netcdf.py
+++ excess_heat_factor/refine_and_buff_netcdf.py
@@ -7,17 +7,20 @@ from shapely.geometry import mapping
 # netcdf so it only contains raster cells that intersects with all the defined
 # SA1 regions from the shapefile.
 
+shapefile = gpd.read_file('..\\_data\\study area\\ausurbhi_study_area_2021.shp', crs="epsg:7844")
 for m in ('max', 'min'):
-    shapefile = gpd.read_file('../_data/study area/ausurbhi_study_area_2021.shp', crs="epsg:7844")
-    netcdf = xr.open_dataset(f'../_data/longpaddock_silo_lst/{m}/2016_2021_{m}_temp.nc')
+    netcdf = xr.open_dataset(f'..\\_data\\AusUrbHI HVI data unprocessed\\Longpaddock SILO LST\\'
+                             f'{m}\\2015_2022_{m}_temp.nc')
     netcdf.rio.set_spatial_dims(x_dim="lon", y_dim="lat", inplace=True)
     netcdf = netcdf.rio.write_crs("EPSG:7844", inplace=True)
 
     # unbuffered version
     clipped = netcdf.rio.clip(shapefile.geometry.apply(mapping), shapefile.crs)
-    clipped.to_netcdf(f'../_data/longpaddock_silo_lst/2016_2021_{m}_temp_clipped_unbuffered.nc')
+    clipped.to_netcdf(f'..\\_data\\AusUrbHI HVI data unprocessed\\Longpaddock SILO LST\\'
+                      f'2015_2022_{m}_temp_clipped_unbuffered.nc')
 
     # buffered version
     shapefile_buffered = shapefile.buffer(0.05)
     clipped = netcdf.rio.clip(shapefile_buffered.geometry.apply(mapping), shapefile.crs)
-    clipped.to_netcdf(f'../_data/longpaddock_silo_lst/2016_2021_{m}_temp_clipped_buffered.nc')
+    clipped.to_netcdf(f'..\\_data\\AusUrbHI HVI data unprocessed\\Longpaddock SILO LST\\'
+                      f'2015_2022_{m}_temp_clipped_buffered.nc')
