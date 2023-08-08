f3fb7427317df60c5055bbca1eb58dccb84f875c
Hao Chen <chen.h@unimelb.edu.au>
2023-08-08 16:55:23 +1000

    add EHF function

--- excess_heat_factor/masking_ehf.py
+++ excess_heat_factor/masking_ehf.py
@@ -1,48 +1,34 @@
-import netCDF4
 import geopandas as gpd
-import numpy as np
-import rasterio
-from rasterio.features import geometry_mask
-from tqdm import tqdm
+import xarray as xr
+import pandas as pd
 
+# Read the input shapefile
+input_shapefile = '..\\_data\\study area\\ausurbhi_study_area_2021.shp'
+gdf = gpd.read_file(input_shapefile, crs="epsg:7844")
 
-def netcdf_to_shp(nc_file, variable, output_path):
-    # Load the NetCDF file
-    nc_data = netCDF4.Dataset(nc_file)
-    ehf_data = nc_data.variables[variable][:]
+# Open the NetCDF file
+nc_file = "..\\_data\\AusUrbHI HVI data unprocessed\\Longpaddock SILO LST\\EHF_heatwaves____daily.nc"
+netcdf = xr.open_dataset(nc_file)
+netcdf.rio.set_spatial_dims(x_dim="lon", y_dim="lat", inplace=True)
+netcdf = netcdf.rio.write_crs("EPSG:7844", inplace=True)
 
-    # Load the shapefile
-    shapefile_path = '..\\_data\\study area\\ausurbhi_study_area_2021.shp'
-    shapes = gpd.read_file(shapefile_path)[:10]
+# Create a DataFrame to store the results
+result_df = pd.DataFrame(columns=['SA1_CODE21', 'values'])
 
-    # Create a list to store the maximum values
-    max_values = []
+# Iterate through the geometries in the shapefile
+for index, row in gdf.iterrows():
+    # Clip the NetCDF file using the geometry
+    clipped_ds = netcdf.rio.clip([row['geometry']])
 
-    # Loop through the geometries in the shapefile
-    for geometry in tqdm(shapes['geometry'], desc='Masking data', total=len(shapes)):
-        # Create a mask from the geometry
-        mask = geometry_mask([geometry], out_shape=ehf_data.shape[1:], transform=rasterio.Affine(1, 0, 0, 0, -1, 0),
-                             invert=True)
+    # Extract the maximum value for each day and store in a list
+    max_values = clipped_ds.max(dim=['x', 'y']).to_dataframe().values.tolist()[365:-365]
 
-        # Apply the mask to the EHF data
-        masked_data = np.ma.array(ehf_data, mask=np.broadcast_to(mask, ehf_data.shape))
+    # Append the results to the DataFrame
+    result_df = result_df.append({'SA1_CODE21': row['SA1_CODE21'], 'values': str(max_values)}, ignore_index=True)
 
-        # Find the maximum value within the geometry
-        max_value = masked_data.max(axis=(1, 2)).tolist()
-        max_values.append(max_value)
+# Merge the results with the original GeoDataFrame
+gdf_result = gdf.merge(result_df, on='SA1_CODE21')
 
-    # Add the maximum values to the shapefile
-    shapes['max_values'] = max_values
-
-    # Convert the maximum values to strings
-    max_values_str = [','.join(map(str, values)) for values in max_values]
-
-    # Add the maximum values to the shapefile
-    shapes['max_values'] = max_values_str
-
-    # Save the new shapefile
-    shapes.to_file(output_path)
-
-
-netcdf_to_shp("..\\_data\\AusUrbHI HVI data unprocessed\\Longpaddock SILO LST\\EHF_heatwaves____daily.nc",
-              "EHF", "..\\_data\\AusUrbHI HVI data processed\\Longpaddock SILO LST\\ehf.shp")
+# Write the new shapefile
+output_shapefile = "..\\_data\\AusUrbHI HVI data processed\\Longpaddock SILO LST\\ehf.shp"
+gdf_result.to_file(output_shapefile)
