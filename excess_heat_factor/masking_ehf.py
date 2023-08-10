446e4e926ab7c8ed7026f360330894b3126b18e0
Hao Chen <chen.h@unimelb.edu.au>
2023-08-10 16:35:56 +1000

    clip nc to generate shp

--- excess_heat_factor/masking_ehf.py
+++ excess_heat_factor/masking_ehf.py
@@ -1,34 +1,20 @@
-import geopandas as gpd
 import xarray as xr
-import pandas as pd
-
-# Read the input shapefile
-input_shapefile = '..\\_data\\study area\\ausurbhi_study_area_2021.shp'
-gdf = gpd.read_file(input_shapefile, crs="epsg:7844")
+import geopandas as gpd
+from shapely.geometry import mapping
+from tqdm import tqdm
 
 # Open the NetCDF file
-nc_file = "..\\_data\\AusUrbHI HVI data unprocessed\\Longpaddock SILO LST\\EHF_heatwaves____daily.nc"
-netcdf = xr.open_dataset(nc_file)
-netcdf.rio.set_spatial_dims(x_dim="lon", y_dim="lat", inplace=True)
-netcdf = netcdf.rio.write_crs("EPSG:7844", inplace=True)
+nc_file = xr.open_dataset(
+    "..\\_data\\AusUrbHI HVI data unprocessed\\Longpaddock SILO LST\\EHF_heatwaves____daily_filled.nc")
+nc_file = nc_file.rio.write_crs("EPSG:7844", inplace=True)
 
-# Create a DataFrame to store the results
-result_df = pd.DataFrame(columns=['SA1_CODE21', 'values'])
+# Open the shapefile
+shapefile = gpd.read_file('..\\_data\\study area\\ausurbhi_study_area_2021.shp')
 
-# Iterate through the geometries in the shapefile
-for index, row in gdf.iterrows():
-    # Clip the NetCDF file using the geometry
-    clipped_ds = netcdf.rio.clip([row['geometry']])
+# Loop through each polygon in the shapefile
+for i, row in tqdm(shapefile.iterrows(), total=len(shapefile), desc="Clipping polygons"):
+    # Clip the NetCDF file by the current polygon
+    clipped_nc = nc_file.rio.clip([mapping(row.geometry)], shapefile.crs, all_touched=True)
 
-    # Extract the maximum value for each day and store in a list
-    max_values = clipped_ds.max(dim=['x', 'y']).to_dataframe().values.tolist()[365:-365]
-
-    # Append the results to the DataFrame
-    result_df = result_df.append({'SA1_CODE21': row['SA1_CODE21'], 'values': str(max_values)}, ignore_index=True)
-
-# Merge the results with the original GeoDataFrame
-gdf_result = gdf.merge(result_df, on='SA1_CODE21')
-
-# Write the new shapefile
-output_shapefile = "..\\_data\\AusUrbHI HVI data processed\\Longpaddock SILO LST\\ehf.shp"
-gdf_result.to_file(output_shapefile)
+    # Save the clipped NetCDF file
+    pass
