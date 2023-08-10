9a87dac67b6a99634e2b3d5fcead9ed1ee667d4a
Hao Chen <chen.h@unimelb.edu.au>
2023-08-10 20:36:44 +1000

    mask merged heatwave netcdf to shapefile

--- excess_heat_factor/masking_ehf.py
+++ excess_heat_factor/masking_ehf.py
@@ -3,6 +3,42 @@ import geopandas as gpd
 import json
 from shapely.geometry import mapping
 from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+
+def process_row(i, row, nc_file, shapefile):
+    # Clip the NetCDF file by the current polygon
+    clipped_nc = nc_file.rio.clip([mapping(row.geometry)], shapefile.crs, all_touched=True)
+
+    # Convert the clipped NetCDF file to a dataframe, with maximum value only rather than all values
+    clipped_df = clipped_nc.to_dataframe().reset_index().groupby(['time']).max().reset_index()
+
+    result = {}
+    # For each column other than "time", "lat", and "lon", create a list of the maximum values, convert to string
+    # and join with a comma, then add to the output shapefile
+    for column in clipped_df.columns:
+        if column not in ["time", "lat", "lon", "spatial_ref"]:
+            value_list = clipped_df[column].astype(str).tolist()
+
+            # For each year, create a field in the output shapefile of the corresponding column values
+            for year in [2016, 2017, 2018, 2019, 2020, 2021]:
+                start, end = year_to_indices(year)
+                value_list_year = value_list[start:end]
+                if "end" not in column:
+                    value_dict = {k: round(float(v), 1) for k, v in enumerate(value_list_year)
+                                  if v != "0 days" and v != "0.0" and v != "0"}
+                else:
+                    value_dict = {k: v for k, v in enumerate(value_list_year)
+                                  if v != "0 days" and v != "0.0" and v != "0"}
+                value = json.dumps(value_dict)
+
+                # Split data into multiple columns if it is too long
+                if value != "{}" and len(value) > 0:
+                    chunks = [value[i:i + 254] for i in range(0, len(value), 254)]
+                    for idx, chunk in enumerate(chunks, 1):
+                        suffix = str(year)[-2:] if idx == 1 else f"{str(year)[-2:]}_{idx}"
+                        result[f"{column}{suffix}"] = chunk
+    return i, result
 
 
 def year_to_indices(year):
@@ -23,38 +59,16 @@ shapefile = gpd.read_file('..\\_data\\study area\\ausurbhi_study_area_2016_with_
 # Create a copy of the shapefile to modify
 output_shapefile = shapefile[['SA1_MAIN16', 'geometry']].copy()
 
-# Loop through each polygon in the shapefile
-for i, row in tqdm(shapefile.iterrows(), total=len(shapefile), desc="Clipping polygons"):
-    # Clip the NetCDF file by the current polygon
-    clipped_nc = nc_file.rio.clip([mapping(row.geometry)], shapefile.crs, all_touched=True)
+# Create a ThreadPoolExecutor
+with ThreadPoolExecutor() as executor:
+    # Submit the tasks to the executor
+    futures = [executor.submit(process_row, i, row, nc_file, shapefile) for i, row in shapefile.iterrows()]
 
-    # convert the clipped NetCDF file to a dataframe, with maximum value only rather than all values
-    clipped_df = clipped_nc.to_dataframe().reset_index().groupby(['time']).max().reset_index()
+    # Collect the results
+    for future in tqdm(as_completed(futures), total=len(shapefile), desc="Clipping polygons"):
+        i, result = future.result()
+        for key, value in result.items():
+            output_shapefile.loc[i, key] = value
 
-    # for each column other than "time", "lat", and "lon", create a list of the maximum values, convert to string
-    # and join with a comma, then add to the output shapefile
-    for column in clipped_df.columns:
-        if column not in ["time", "lat", "lon", "spatial_ref"]:
-            value_list = clipped_df[column].astype(str).tolist()
-
-            # for each year, create a field in the output shapefile of the corresponding column values
-            for year in [2016, 2017, 2018, 2019, 2020, 2021]:
-                start, end = year_to_indices(year)
-                value_list_year = value_list[start:end]
-                if "end" not in column:
-                    value_dict = {k: round(float(v), 1) for k, v in enumerate(value_list_year)
-                                  if v != "0 days" and v != "0.0" and v != "0"}
-                else:
-                    value_dict = {k: v for k, v in enumerate(value_list_year)
-                                  if v != "0 days" and v != "0.0" and v != "0"}
-                value = json.dumps(value_dict)
-
-                # split data into multiple columns if it is too long
-                if value != "{}" and len(value) > 0:
-                    chunks = [value[i:i + 254] for i in range(0, len(value), 254)]
-                    for idx, chunk in enumerate(chunks, 1):
-                        suffix = str(year)[-2:] if idx == 1 else f"{str(year)[-2:]}_{idx}"
-                        output_shapefile.loc[i, f"{column}{suffix}"] = chunk
-
-# save the shapefile
+# Save the shapefile
 output_shapefile.to_file(f"..\\_data\\AusUrbHI HVI data processed\\Longpaddock SILO LST\\merged_heatwaves.shp")
