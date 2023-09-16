23437114ec26f3d57ef576fc9a3eae249b8d6754
Hao Chen <chen.h@unimelb.edu.au>
2023-09-16 19:33:22 +1000

    updated 2016 to 2021 concordance scripts with quality information from concordance csv

--- 2016_to_2021_concordance/concordance_mapper.py
+++ 2016_to_2021_concordance/concordance_mapper.py
@@ -4,17 +4,8 @@ from tqdm import tqdm
 
 
 class ConcordanceMapper:
-    def __init__(self,
-                 csv_df,
-                 study_area_df,
-                 csv_16_column,
-                 csv_21_column,
-                 filename,
-                 file_path,
-                 shp_16_field,
-                 shp_21_field,
-                 exclude_division_field_list,
-                 output_folder_path):
+    def __init__(self, csv_df, study_area_df, csv_16_column, csv_21_column, filename, file_path,
+                 shp_16_field, shp_21_field, exclude_division_field_list, output_folder_path):
         self.csv_df = csv_df
         self.study_area_df = study_area_df
         self.csv_16_column = csv_16_column
@@ -27,40 +18,26 @@ class ConcordanceMapper:
         self.output_folder_path = output_folder_path
 
     def map(self):
-        # Read the CSV file into a DataFrame
         self.csv_df.dropna(subset=[self.csv_16_column], inplace=True)
         self.csv_df[self.csv_16_column] = self.csv_df[self.csv_16_column].astype(float).astype('int64').astype(str)
 
-        # Read the Shapefile into a GeoDataFrame
         gdf = gpd.read_file(self.file_path)
+        new_gdf = gpd.GeoDataFrame(columns=list(gdf.columns) + ['INDIV_QLTY', 'OVR_QLTY'])
 
-        # Create an empty GeoDataFrame to store processed rows
-        new_gdf = gpd.GeoDataFrame(columns=gdf.columns)
-
-        # Iterate through each row in the Shapefile
         for idx, row in tqdm(gdf.iterrows(), total=len(gdf), desc=f"processing {self.filename}"):
             sa1_2016 = str(row[self.shp_16_field])
-
-            # Check if sa1_2016 exists in the csv file
             assert sa1_2016 in self.csv_df[self.csv_16_column].values
-
-            # Find corresponding 2021 codes and their ratios
             matches = self.csv_df[self.csv_df[self.csv_16_column] == sa1_2016]
 
             if len(matches) > 1:
                 for _, match in matches.iterrows():
                     new_row = row.copy()
-
-                    # Replace SA1_MAIN16 and update geometry
                     sa1_2021 = match[self.csv_21_column]
                     new_row[self.shp_16_field] = sa1_2021
                     if sa1_2021 in self.study_area_df[self.shp_21_field].values:
                         new_row['geometry'] = \
                             self.study_area_df[self.study_area_df[self.shp_21_field] == sa1_2021].iloc[0]['geometry']
-                    else:
-                        pass
 
-                    # Update other columns
                     for col in new_row.index:
                         if col not in self.exclude_division_field_list:
                             try:
@@ -69,14 +46,15 @@ class ConcordanceMapper:
                             except ValueError:
                                 pass
 
-                    # Append the new row to the new GeoDataFrame
-                    new_gdf = new_gdf._append(new_row)
+                    new_row['INDIV_QLTY'] = match['INDIV_TO_REGION_QLTY_INDICATOR']
+                    new_row['OVR_QLTY'] = match['OVERALL_QUALITY_INDICATOR']
+                    new_gdf = new_gdf._append(new_row, ignore_index=True)
             else:
-                new_gdf = new_gdf._append(row)
+                row['INDIV_QLTY'] = matches.iloc[0]['INDIV_TO_REGION_QLTY_INDICATOR']
+                row['OVR_QLTY'] = matches.iloc[0]['OVERALL_QUALITY_INDICATOR']
+                new_gdf = new_gdf._append(row, ignore_index=True)
 
-        # Save the new GeoDataFrame to a new Shapefile
         new_gdf.rename(columns={self.shp_16_field: self.shp_21_field}, inplace=True)
         output_filename = self.filename.replace('.shp', '_2021_concordance.shp')
-        output_path = os.path.join(self.output_folder_path,
-                                   output_filename)
+        output_path = os.path.join(self.output_folder_path, output_filename)
         new_gdf.to_file(output_path)
