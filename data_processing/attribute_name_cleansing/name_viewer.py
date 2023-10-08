3329b5de4f0b3902888bf5b77b5056541a153ff6
Hao Chen <chen.h@unimelb.edu.au>
2023-10-09 02:26:26 +1100

    bug fix for disaggregation algorithms; add shp attribute name viewer

--- data_processing/attribute_name_cleansing/name_viewer.py
+++ data_processing/attribute_name_cleansing/name_viewer.py
@@ -1,6 +1,6 @@
 import shapefile
 
-file = "au-govt-abs-census-sa1-g59-method-of-travel-to-work-by-sex-census-2016-sa1-2016_study_area_refined_2021_concordance.shp"
+file = "datasource-AU_Govt_ABS_Census-UoM_AURIN_DB_2_sa1_g39_dwelling_struct_by_hsehold_and_family_census_2016_study_area_refined_2021_concordance.shp"
 path = "../../_data/AusUrbHI HVI data processed/ABS 2016 by 2021 concordance/" + file
 
 sf = shapefile.Reader(path)
