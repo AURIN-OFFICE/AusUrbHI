acf20809b51bad39fece1c25ed1b31675fb76e4a
Hao Chen <chen.h@unimelb.edu.au>
2023-10-15 14:03:06 +1100

    reorganize folders

--- data_processing/attribute_name_cleansing/name_viewer.py
+++
@@ -1,8 +0,0 @@
-import shapefile
-
-file = "datasource-AU_Govt_ABS_Census-UoM_AURIN_DB_2_sa1_g39_dwelling_struct_by_hsehold_and_family_census_2016_study_area_refined_2021_concordance.shp"
-path = "../../_data/AusUrbHI HVI data processed/ABS 2016 by 2021 concordance/" + file
-
-sf = shapefile.Reader(path)
-field_names = [field[0] for field in sf.fields[1:]]  # Skip the deletion flag
-print(field_names)
---
+++ geoscape/buildings.py
@@ -0,0 +1,4 @@
+
+
+path = "..\_data\AusUrbHI HVI data unprocessed\Geospace\Buildings_JUN23_NSW_GDA94_SHP_317\Buildings\
+Buildings JUNE 2023\Standard\nsw_buildings.shp"
