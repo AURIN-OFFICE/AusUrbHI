bbff65e7e60b940e33021cd7decc0867cbe01b43
Hao Chen <chen.h@unimelb.edu.au>
2023-10-17 00:50:58 +1100

    Geoscape buildings processing

--- geoscape/scripts/refine_buildings.py
+++ geoscape/scripts/refine_buildings.py
@@ -5,7 +5,7 @@ cols_meshblock = ['MB_CODE21', 'SA1_CODE21']
 meshblock_gdf = gpd.read_file("../../_data/study area/meshblock_study_area_2021.shp",
                               usecols=cols_meshblock,
                               ignore_geometry=True)
-cols_buildings = ['MB_CODE', 'SA1_CODE21', 'SP_ADJ', 'ROOF_HGT', 'PR_RF_MA', 'AREA', 'EST_LEV']
+cols_buildings = ['MB_CODE', 'SP_ADJ', 'ROOF_HGT', 'PR_RF_MA', 'AREA', 'EST_LEV']
 buildings_gdf = gpd.read_file("../../_data/AusUrbHI HVI data unprocessed/Geoscape/"
                               "Buildings_JUN23_NSW_GDA94_SHP_317/Buildings/Buildings JUNE 2023/"
                               "Standard/nsw_buildings.shp",
@@ -18,7 +18,7 @@ study_area_meshblock_codes = set(meshblock_gdf['MB_CODE21'])
 buildings_gdf = buildings_gdf[buildings_gdf['MB_CODE'].isin(study_area_meshblock_codes)]
 print("refined buildings data to study area.")
 
-# Add SA1 code
+# Add SA1 code as field to building datasd
 meshblock_sa1_code_dict = meshblock_gdf.set_index('MB_CODE21')['SA1_CODE21'].to_dict()
 buildings_gdf['SA1_CODE21'] = buildings_gdf['MB_CODE'].map(meshblock_sa1_code_dict)
 print("added SA1 code to buildings data.")
