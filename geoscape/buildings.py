ffb61cfb714b232449fac5b39a8eda4de6938001
Hao Chen <chen.h@unimelb.edu.au>
2023-10-16 23:05:04 +1100

    Geoscape buildings processing

--- geoscape/buildings.py
+++ geoscape/buildings.py
@@ -1,94 +1,57 @@
 import geopandas as gpd
-import pandas as pd
-import fiona
 from tqdm import tqdm
+from geopandas import GeoDataFrame
 
 
 class StudyAreaProcessor:
-    def __init__(self, building_path, meshblock_path, study_area_path, rows=None):
-        self.meshblock_gdf = gpd.read_file(meshblock_path)
-        print("Meshblock data loaded.")
+    def __init__(self, meshblock_gdf, study_area_gdf, building_gdf):
+        self.meshblock_gdf = meshblock_gdf
+        self.study_area_gdf = study_area_gdf
+        self.building_gdf = building_gdf
 
-        self.study_area_gdf = gpd.read_file(study_area_path)
-        print("Study area data loaded.")
+    def preprocessing(self):
+        # create a dictionary of SA1 area
+        self.sa1_area_dict = self.study_area_gdf.set_index('SA1_CODE21')['AREASQKM21'].to_dict()
 
-        if rows:
-            with fiona.open(building_path) as source:
-                features_subset = [next(iter(source)) for _ in range(rows)]
-            self.building_gdf = gpd.GeoDataFrame.from_features(features_subset, crs=source.crs)
-        else:
-            self.building_gdf = gpd.read_file(building_path)
-        print("Building data loaded.")
+        # group buildings by SA1
+        self.grouped_buildings = self.building_gdf.groupby('SA1_CODE21')
+        assert len(self.grouped_buildings.groups) == len(
+            self.study_area_gdf), "Number of SA1s in study area does not match number of SA1s in building data."
 
-        self.study_buildings_data_dict = {}
+    def compute_fields(self):
+        self.result = []
+        for sa1_code, group_data in tqdm(self.grouped_buildings, total=len(self.grouped_buildings),
+                                         desc="processing buildings"):
+            count = len(group_data)
+            sp_adj_yes = group_data['SP_ADJ'].value_counts().get('yes', 0)
+            pr_rf_ma_counts = group_data['PR_RF_MA'].value_counts()
 
-    def _aggregate_buildings(self):
-        meshblock_sa1_code_dict = self.meshblock_gdf.set_index('MB_CODE21')['SA1_CODE21'].to_dict()
-        grouped = self.building_gdf.groupby(self.building_gdf["MB_CODE"].map(meshblock_sa1_code_dict))
-        for column in ["SP_ADJ", "ROOF_HGT", "PR_RF_MAT", "AREA", "EST_LEV"]:
-            self.study_buildings_data_dict[column] = grouped[column].apply(
-                lambda x: [item for item in list(x) if item is not None and item == item and item != '']).to_dict()
-        print("Aggregation of buildings is done.")
+            # compute fields based on values of all buildings in the SA1
+            sa1_dict = {'SA1_CODE21': sa1_code,
+                        'SP_ADJ': sp_adj_yes / count,
+                        'ROOF_HGT': group_data['ROOF_HGT'].mean(),
+                        'MAT_Tile': pr_rf_ma_counts.get('Tile', 0) / count,
+                        'MAT_Metal': pr_rf_ma_counts.get('Metal', 0) / count,
+                        'MAT_Concre': pr_rf_ma_counts.get('Flat Concrete', 0) / count,
+                        'AREA': group_data['AREA'].sum() / self.sa1_area_dict[sa1_code] * 1e6,
+                        'EST_LEV': group_data['EST_LEV'].mean()}
+            self.result.append(sa1_dict)
 
-    def _compute_shapefile_fields_for_sa1(self, sa1_range):
-        sa1_df = self.study_area_gdf.iloc[sa1_range].copy()
-
-        sa1_df['SP_ADJ'] = sa1_df['SA1_CODE21'].apply(
-            lambda x: self.study_buildings_data_dict['SP_ADJ'][x].count('yes') / len(
-                self.study_buildings_data_dict['SP_ADJ'][x]) if x in self.study_buildings_data_dict['SP_ADJ'] else 0)
-
-        sa1_df['ROOF_HGT'] = sa1_df['SA1_CODE21'].apply(
-            lambda x: sum(map(float, self.study_buildings_data_dict['ROOF_HGT'][x])) / len(
-                self.study_buildings_data_dict['ROOF_HGT'][x]) if x in self.study_buildings_data_dict[
-                'ROOF_HGT'] else 0)
-
-        for mat, col in [("Tile", "MAT_Tile"), ("Metal", "MAT_Metal"), ("Flat Concrete", "MAT_Concre")]:
-            sa1_df[col] = sa1_df['SA1_CODE21'].apply(
-                lambda x: self.study_buildings_data_dict['PR_RF_MAT'][x].count(mat) / len(
-                    self.study_buildings_data_dict['PR_RF_MAT'][x]) if x in self.study_buildings_data_dict[
-                    'PR_RF_MAT'] else 0)
-
-        sa1_df['AREA'] = sa1_df.apply(
-            lambda row: sum(map(float, self.study_buildings_data_dict['AREA'][row['SA1_CODE21']])) / (
-                        row['AREASQKM21'] * 1e6) if row['SA1_CODE21'] in self.study_buildings_data_dict['AREA'] else 0,
-            axis=1)
-
-        sa1_df['EST_LEV'] = sa1_df['SA1_CODE21'].apply(
-            lambda x: sum(self.study_buildings_data_dict['EST_LEV'][x]) / len(
-                self.study_buildings_data_dict['EST_LEV'][x]) if x in self.study_buildings_data_dict['EST_LEV'] else 0)
-
-        return sa1_df
-
-    def _compute_shapefile_fields(self):
-        dfs = []
-        sa1_size = 100  # you can adjust this as needed
-        total_sa1s = (len(self.study_area_gdf) + sa1_size - 1) // sa1_size
-
-        for i in tqdm(range(total_sa1s)):
-            start_idx = i * sa1_size
-            end_idx = min((i + 1) * sa1_size, len(self.study_area_gdf))
-            dfs.append(self._compute_shapefile_fields_for_sa1(range(start_idx, end_idx)))
-
-        print("Shapefile field computation is done.")
-        return pd.concat(dfs, ignore_index=True)
-
-    def save_shapefile(self, output_path):
-        self._aggregate_buildings()
-        result_gdf = self._compute_shapefile_fields()
-        result_gdf = result_gdf[
-            ['geometry', 'SA1_CODE21', 'SP_ADJ', 'ROOF_HGT', 'MAT_Tile',
-             'MAT_Metal', 'MAT_Concre', 'AREA', 'EST_LEV']]
-        result_gdf.to_file(output_path)
-        print("Shapefile saved.")
+    def save_output(self, output_path):
+        new_gdf = GeoDataFrame(self.result, crs=self.study_area_gdf.crs)
+        new_gdf = new_gdf.merge(self.study_area_gdf[['SA1_CODE21', 'geometry']], on='SA1_CODE21', how='left')
+        new_gdf.to_file(output_path)
 
 
 if __name__ == "__main__":
-    processor = StudyAreaProcessor(
-        # building_path="../_data/AusUrbHI HVI data processed/Geoscape/temporary/buildings_in_study_area.shp"
-        building_path="../_data/AusUrbHI HVI data unprocessed/Geospace/Buildings_JUN23_NSW_GDA94_SHP_317/Buildings/"
-                      "Buildings JUNE 2023/Standard/nsw_buildings.shp",
-        meshblock_path="../_data/study area/meshblock_study_area_2021.shp",
-        study_area_path="../_data/study area/ausurbhi_study_area_2021.shp",
-        rows=100000
-    )
-    processor.save_shapefile('../_data/AusUrbHI HVI data processed/Geoscape/buildings.shp')
+    building_gdf = gpd.read_file("../_data/AusUrbHI HVI data processed/Geoscape/temporary/buildings_in_study_area.shp")
+    meshblock_gdf = gpd.read_file("../_data/study area/meshblock_study_area_2021.shp")
+    study_area_gdf = gpd.read_file("../_data/study area/ausurbhi_study_area_2021.shp")
+    print("data loaded.")
+
+    processor = StudyAreaProcessor(meshblock_gdf, study_area_gdf, building_gdf)
+    processor.preprocessing()
+    print("Grouping done.")
+    processor.compute_fields()
+    print("Computing done.")
+    processor.save_output('../_data/AusUrbHI HVI data processed/Geoscape/buildings.shp')
--- geoscape/scripts/refine_buildings.py
+++ geoscape/scripts/refine_buildings.py
@@ -1,11 +1,29 @@
 import geopandas as gpd
 
-meshblock_gdf = gpd.read_file("../../_data/study area/meshblock_study_area_2021.shp")
-buildings_gdf = gpd.read_file("../../_data/AusUrbHI HVI data unprocessed/Geospace/Buildings_JUN23_NSW_GDA94_SHP_317/"
-                              "Buildings/Buildings JUNE 2023/Standard/nsw_buildings.shp")
+# Load only necessary columns
+cols_meshblock = ['MB_CODE21', 'SA1_CODE21']
+meshblock_gdf = gpd.read_file("../../_data/study area/meshblock_study_area_2021.shp",
+                              usecols=cols_meshblock,
+                              ignore_geometry=True)
+cols_buildings = ['MB_CODE', 'SA1_CODE21', 'SP_ADJ', 'ROOF_HGT', 'PR_RF_MA', 'AREA', 'EST_LEV']
+buildings_gdf = gpd.read_file("../../_data/AusUrbHI HVI data unprocessed/Geoscape/"
+                              "Buildings_JUN23_NSW_GDA94_SHP_317/Buildings/Buildings JUNE 2023/"
+                              "Standard/nsw_buildings.shp",
+                              usecols=cols_buildings,
+                              ignore_geometry=True)
+print("data loaded.")
 
+# refine entries to study area using set intersection
 study_area_meshblock_codes = set(meshblock_gdf['MB_CODE21'])
-filtered_meshblock_gdf = buildings_gdf[buildings_gdf['MB_CODE'].isin(study_area_meshblock_codes)]
+buildings_gdf = buildings_gdf[buildings_gdf['MB_CODE'].isin(study_area_meshblock_codes)]
+print("refined buildings data to study area.")
 
+# Add SA1 code
+meshblock_sa1_code_dict = meshblock_gdf.set_index('MB_CODE21')['SA1_CODE21'].to_dict()
+buildings_gdf['SA1_CODE21'] = buildings_gdf['MB_CODE'].map(meshblock_sa1_code_dict)
+print("added SA1 code to buildings data.")
+
+# Save
 output_path = "../../_data/AusUrbHI HVI data processed/Geoscape/temporary/buildings_in_study_area.shp"
-filtered_meshblock_gdf.to_file(output_path)
+buildings_gdf.to_file(output_path)
+print("saved to", output_path)
