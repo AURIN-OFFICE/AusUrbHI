6fa6d77ec1731fcff64540645dd12964aa91ed18
Hao Chen <chen.h@unimelb.edu.au>
2023-10-19 20:04:47 +1100

    add cleansing to version control

---
+++ cleansing/get_sa1_population_csv.py
@@ -0,0 +1,18 @@
+import geopandas as gpd
+
+abs_2016_g01_gdf = gpd.read_file("../_data/AusUrbHI HVI data processed/ABS 2016/"
+                                 "datasource-AU_Govt_ABS_Census-UoM_AURIN_DB_2_sa1_g01_selected_person_characteristics_by_sex_census_2016_study_area_refined.shp")
+abs_2021_g01_gdf = gpd.read_file("../_data/AusUrbHI HVI data processed/ABS 2021/main_G01_SA1_2021_NSW_study_area_refined.shp")
+abs_2016_field = ["SA1_MAIN16", "tot_p"]
+abs_2021_field = ["SA1_CODE21", "Tot_P_P"]
+
+# create two csv files with two columns: SA1 code and population
+abs_2016_g01_gdf = abs_2016_g01_gdf[abs_2016_field]
+abs_2016_g01_gdf.columns = ["SA1_CODE", "population"]
+abs_2016_csv_path = "../_data/abs_2016_population.csv"
+abs_2016_g01_gdf.to_csv(abs_2016_csv_path, index=False)
+
+abs_2021_g01_gdf = abs_2021_g01_gdf[abs_2021_field]
+abs_2021_g01_gdf.columns = ["SA1_CODE", "population"]
+abs_2021_csv_path = "../_data/abs_2021_population.csv"
+abs_2021_g01_gdf.to_csv(abs_2021_csv_path, index=False)
---
+++ cleansing/liveability_spatialization.py
@@ -0,0 +1,17 @@
+import geopandas as gpd
+import pandas as pd
+
+# Read the csv and shapefile data
+csv_data = pd.read_csv(r'..\_data\AusUrbHI HVI data unprocessed\Liveability\LiveabilityIndex_sydney_2021_sa1.csv')
+shp_data = gpd.read_file(r'..\_data\study area\ausurbhi_study_area_2021.shp')
+
+# Convert the merge columns to string type
+csv_data['sa1'] = csv_data['sa1'].astype(str)
+shp_data['SA1_CODE21'] = shp_data['SA1_CODE21'].astype(str)
+
+# Merge the csv data with the shapefile based on the 'sa1' and 'SA1_CODE21' fields
+merged_data = shp_data.merge(csv_data, left_on='SA1_CODE21', right_on='sa1')
+
+# Save the merged data to a new shapefile
+output_path = r'..\_data\AusUrbHI HVI data processed\Liveability\LiveabilityIndex_sydney_2021_sa1.shp'
+merged_data.to_file(output_path)
