a800cb4f31c0c47cc46d640c597e1d0dbffefac4
Hao Chen <chen.h@unimelb.edu.au>
2023-11-01 15:40:30 +1100

    added excess heat days analysis

--- heatwave_identification/ehf_netcdf_analyzer.py
+++ heatwave_identification/ehf_netcdf_analyzer.py
@@ -10,11 +10,10 @@ class EHFAnalyzer:
 
     def __init__(self):
         self.sa1_centroid_dict = self._creat_sa1_centroid_dict()
-        self.ds = xr.open_dataset('../_data/AusUrbHI HVI data unprocessed/Longpaddock SILO LST/'
-                                  'EHF_heatwaves____daily.nc')
+        self.ehf_xarray = xr.open_dataset('../_data/AusUrbHI HVI data unprocessed/Longpaddock SILO LST/'
+                                          'EHF_heatwaves____daily.nc')
+        self.lst_xarray = None
         self.output_gdf = gpd.read_file('../_data/study area/ausurbhi_study_area_2021.shp')[['SA1_CODE21', 'geometry']]
-        self.heatwave_days = None
-        self.extreme_heatwave_days = None
 
     @staticmethod
     def _creat_sa1_centroid_dict() -> dict:
@@ -27,7 +26,7 @@ class EHFAnalyzer:
         return sa1_centroid_dict
 
     @staticmethod
-    def _compute_average_heatwave_duration(point_data_ends: xr.DataArray) -> int:
+    def _compute_average_heatwave_duration(point_data_ends: xr.DataArray) -> float:
         """The input is a heatwave length xarray data like [0, 3, 0, 0, 0, 2, 0, 0]. The function finds the average
         non-zero values in the list. In this case 2.5. The hard part is dealing with timedelta and nan values.
         """
@@ -58,16 +57,17 @@ class EHFAnalyzer:
 
         return sum(segment_lengths) / len(segment_lengths) if segment_lengths else 0
 
-    def ehf_statistics_analysis(self, start_year: int = 11, end_year: int = 22) -> None:
+    def ehf_statistics_analysis(self, years: list = (16, 21)) -> None:
         """Compute for each year and SA1:
         1. the average and maximum EHF,
-        2. average excess heat duration and number of excess heat days (EHF > 0),
-        3. the average heatwave duration, number of heatwave days, and
-        4. number of extreme heatwave days
+        2. the average heatwave duration, number of heatwave days,
+        3. average excess heat duration and number of excess heat days (EHF > 0),
+        4. number of extreme heatwave days, and
+        5. average and maximum summer percentile deviation
         for each SA1 centroid location during summer months (Dec, Jan, Feb) in each year.
         """
-        for year in range(start_year, end_year + 1):
-            ds_summer = self.ds.sel(time=slice(f'20{year-1}-12-01', f'20{year}-02-28'))
+        for year in years:
+            ds_summer = self.ehf_xarray.sel(time=slice(f'20{year-1}-12-01', f'20{year}-02-28'))
 
             # initiate new fields; explains will be given below
             average_EHF_value = f'avg_ehf_{year}'
@@ -80,8 +80,9 @@ class EHFAnalyzer:
             number_of_excess_heat_days = f'eh_days_{year}'
 
             number_of_extreme_heatwave_days = f'ex_len_{year}'
-            for i in [average_EHF_value, max_EHF_value, average_heatwave_duration, number_of_heatwave_days]:
-                self.output_gdf[i] = pd.NA
+
+            average_summer_percentile_deviation = None
+            maximum_summer_percentile_deviation = None
 
             # get relevant data for each SA1 centroid location during the period
             year_data = {}
@@ -101,10 +102,13 @@ class EHFAnalyzer:
                 # average_EHF_value: average EHF values
                 # max_EHF_value: max EHF value
                 # average_heatwave_duration: average of non-zero values in point_data['ends']
-                # number_of_heatwave_days: number of non-zero values in point_data['event']
+                # number_of_heatwave_days: number/sum of non-zero values in point_data['event']
                 # average_excess_heat_duration: average lengths of non-zero consecutive values in point_data['EHF']
                 # number_of_excess_heat_days: number of non-zero EHF values
                 # number_of_extreme_heatwave_days: number of non-zero event value while also EHF >= 3
+
+                assert (point_data['event'].sum().item() ==
+                        (point_data['event'] != 0).sum().item(), 'event stats error')
 
                 year_data[sa1] = {
                     average_EHF_value: round(point_data['EHF'].mean().item(), 2),
@@ -119,12 +123,18 @@ class EHFAnalyzer:
 
                     number_of_extreme_heatwave_days: round(
                         ((point_data['event'] == 1) & (point_data['EHF'] >= 3)).sum().item(), 2)
+
+                    average_summer_percentile_deviation: None,
+                    maximum_summer_percentile_deviation: None
                 }
 
             # add the data to the output shapefile in the new added fields
             for sa1_code, values in year_data.items():
-                for i in [average_EHF_value, max_EHF_value, average_excess_heat_duration, number_of_excess_heat_days,
-                          average_heatwave_duration, number_of_heatwave_days, number_of_extreme_heatwave_days]:
+                for i in [average_EHF_value, max_EHF_value,
+                          average_excess_heat_duration, number_of_excess_heat_days,
+                          average_heatwave_duration, number_of_heatwave_days,
+                          number_of_extreme_heatwave_days,
+                          average_summer_percentile_deviation, maximum_summer_percentile_deviation]:
                     self.output_gdf.loc[self.output_gdf['SA1_CODE21'] == sa1_code, i] = values[i]
 
     def get_all_heatwave_days(self, start_year: int = 11, end_year: int = 22) -> None:
@@ -133,13 +143,13 @@ class EHFAnalyzer:
         2. the dates of extreme heatwaves (EHF >=3)
         for each SA1 centroid location during summer months (Dec, Jan, Feb) in each year.
         """
-        # Ensure self.ds['time'] is a DataArray and convert its values
+        # Ensure self.ehf_xarray['time'] is a DataArray and convert its values
 
         heatwave_days = defaultdict(dict)
         extreme_heatwave_days = defaultdict(dict)
 
         for year in range(start_year, end_year + 1):
-            ds_summer = self.ds.sel(time=slice(f'20{year - 1}-12-01', f'20{year}-02-28'))
+            ds_summer = self.ehf_xarray.sel(time=slice(f'20{year - 1}-12-01', f'20{year}-02-28'))
 
             for sa1, centroid in tqdm(self.sa1_centroid_dict.items(),
                                       total=len(self.sa1_centroid_dict),
